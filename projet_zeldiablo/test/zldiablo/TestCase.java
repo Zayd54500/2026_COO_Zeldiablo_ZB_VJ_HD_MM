1e55fa2a014b5b9ef9010bd2d4b0a8bbc6291ee3
Dodler Hercule <e29415u@etu.univ-lorraine.fr>
2026-06-02 14:58:05 +0200

    ajout du testCase

---
+++ projet_zeldiablo/test/zldiablo/TestCase.java
@@ -0,0 +1,35 @@
+package zldiablo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestCase {
+
+@Test
+public void testCaseVidetraversable(){
+    CaseVide caseVide = new Case Vide();
+
+assertTrue("Une case vide doit être traversable", caseVide.etreTraversable());
+}
+
+@Test 
+public void testCaseVideNom(){
+    CaseVide caseVide = new CaseVide();
+
+assertEquals("vide", caseVide.getNom());
+}
+
+@Test 
+public void testMurNonTraversable(){
+    Mur mur = new Mur();
+
+assertFalse("Un mur ne doit pas être traversable", mur.etreTraversable());
+}
+
+@Test
+public void testMurNom(){
+    Mur mur = new Mur();
+
+assertEquals("mur", mur.getNom());
+}
+}
