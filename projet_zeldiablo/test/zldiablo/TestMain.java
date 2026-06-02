7ea90e2b8569a9fba2ae9332818ce4eaedb22c21
Andaloussi212 <zayd.benz.pro@proton.me>
2026-06-02 15:05:05 +0200

    TestMain

--- projet_zeldiablo/test/zldiablo/TestMain.java
+++ projet_zeldiablo/test/zldiablo/TestMain.java
@@ -3,18 +3,31 @@ package zldiablo;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class TestMain {
     @Test
-    // verification que retour retourne bien 1
-    public void testRetour() {
-        // preparation des donnees
-        Main m = new Main();
-        // appel de la methode
-        int val = m.retour();
-        // verifications
-        assertEquals("devrait etre egal à 1", 1, val);
+    public void testCaseVideTraversable() {
+        CaseVide vide = new CaseVide();
+        assertTrue("Une case vide doit être traversable", vide.etreTraversable());
     }
 
+    @Test
+    public void testCaseVideNom() {
+        CaseVide vide = new CaseVide();
+        assertEquals("vide", vide.getNom());
+    }
+
+    @Test
+    public void testMurNonTraversable() {
+        Mur mur = new Mur();
+        assertFalse("Un mur ne doit pas être traversable", mur.etreTraversable());
+    }
+
+    @Test
+    public void testMurNom() {
+        Mur mur = new Mur();
+
+        assertEquals("mur", mur.getNom());
+    }
 }
