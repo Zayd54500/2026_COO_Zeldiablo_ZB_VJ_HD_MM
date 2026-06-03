4a9c29f2646dc5ef2222e66785368b552e48ae44
Andaloussi212 <zayd.benz.pro@proton.me>
2026-06-03 15:59:26 +0200

    Ajout de la javaDoc

--- projet_zeldiablo/src/zldiablo/Case.java
+++ projet_zeldiablo/src/zldiablo/Case.java
@@ -1,8 +1,37 @@
 package zldiablo;
 
+/**
+ * Représente une case du labyrinthe
+ *
+ * Une case peut être un mur, une case vide, un piège ou une pièce mais aussi une sortie
+ * Chaque type de case définis son comportement : traversable ou non, symbole utilisé pour l'affichage
+ */
 public interface Case {
+    /**
+     * Retourne le caractère représentant le type de la case
+     *
+     * @return le caractère associé à la case
+     */
     public boolean etreTraversable();
+
+    /**
+     * Retourne le caractère représentant le type de la case.
+     *
+     * @return le caractère associé à la case.
+     */
     public char getNom();
+
+    /**
+     * Indique si la case a déjà été prise
+     *
+     * @return true si la case a déjà été prise, false sinon
+     */
     public void etrePris();
+
+    /**
+     * Indique si la case a déjà été prise.
+     *
+     * @return true si la case a déjà été prise, false sinon.
+     */
     public boolean getPris();
 }
